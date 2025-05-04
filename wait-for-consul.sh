b7fdea20af5997a7c38b1569931ecd812808f41b
Pradip Acharjee <pacharjee@atlassian.com>
2025-05-04 21:45:44 +0530

    Removed wait-for-consul script.

--- wait-for-consul.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-CONSUL_HOST=$1
-shift
-until curl -s $CONSUL_HOST/v1/status/leader | grep -q '\"'; do
-  >&2 echo "Consul is unavailable - sleeping"
-  sleep 5
-done
->&2 echo "Consul is up - executing command"
-exec "$@"
